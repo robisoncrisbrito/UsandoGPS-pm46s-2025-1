0ad349ed3a8d404d0aa3a650aa1a1ffcdab46275
robisoncrisbrito <robison.brito@gmail.com>
2025-03-27 15:05:07 -0300

    projeto completo

--- app/src/main/java/br/edu/utfpr/usandogps_pm46s_2025_1/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandogps_pm46s_2025_1/MainActivity.kt
@@ -1,20 +1,62 @@
 package br.edu.utfpr.usandogps_pm46s_2025_1
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    private lateinit var lm : LocationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        lm = getSystemService( Context.LOCATION_SERVICE ) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
         }
+
+        lm.requestLocationUpdates( LocationManager.NETWORK_PROVIDER, 0, 0f, this  )
+
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+
+        val latitude = location.latitude
+        val longitude = location.longitude
+
+        val tvLatitude : TextView = findViewById( R.id.tvLatitude )
+        val tvLongitude : TextView = findViewById( R.id.tvLongitude )
+
+        tvLatitude.text = latitude.toString()
+        tvLongitude.text = longitude.toString()
+
     }
 }
